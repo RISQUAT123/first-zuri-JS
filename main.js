19a66f29bbbc9757253bce8e043b5c9057de4434
Adekoya Risquat <risquatpelumi@gmail.com>
2022-06-24 20:26:32 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const firstName = 'Risquat';
+const lastName = 'Adekoya';
+const height = '4ft';
+const country = 'Nigerian';
+
+val = 'My name is' + ' ' + firstName + ' ' + lastName + ' ' +
+    'i am ' + height + ' ' +
+    'tall and i am a' +
+    ' ' + country;
+
+window.alert(val);
